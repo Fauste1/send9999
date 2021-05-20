72b5919f60642dfeb9d9cd3f69586221c44d0b95
Štěpán Fau <sbren@Hexsus1.local>
2021-05-20 23:04:45 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+BREAKING: The U.S. Treasury said the Biden administration’s proposal to strengthen tax compliance includes a requirement for transfers of at least $10,000 of cryptocurrency to be reported to the IRS https://trib.al/ffIuhq5
+
+I'm pretty sure you know what this app is designed to do.
+
+App is currently in a dev mode, nothing has been tested. I strongly discourage using the app or the smart contracts, especially with real money.
+
+The code is fully open source. Feel free to fork it, do whatever you want with it. I'll figure out the licence terms later.
---
+++ app.js
@@ -0,0 +1,16 @@
+// Connect Metamask
+
+// Get the address of the sender
+// Get the address of the recipient
+// Get the total amount of ETH to be sent
+
+// Get the current price of ETH
+    // How does the IRS decide what was the price at the moment of the transfer?
+// Split the TX into smaller chunks, each below $10k
+    // What's the minimum buffer a TX should have in order for the price not to exceed 10k?
+
+// Prepare the TXs
+    // How to handle a failure of one of the TXs?
+// Sign the TXs
+    // have to figure out how to work with Metamask (and other wallets) and signing without revealing the user's private key
+// Broadcast the TXs
